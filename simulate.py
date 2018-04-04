be630144df47311874d329a02049c1825bc53404
J. David Giese <johndgiese@gmail.com>
2018-04-04 17:50:09 -0400

    Split paramaters into logical groups

--- simulate.py
+++ simulate.py
@@ -7,18 +7,18 @@ def simulate(params):
     for k in range(params["sensor_width"]):
         start = k + s/2
         stop = k + 1 - s/2
-        result = integrate_sensor(params, start, stop)
+        result = integrate_sensor(params["image_params"], start, stop)
         result_with_noise = add_noise(params, result)
         image[k] = digitize(params, result_with_noise)
     return image
 
 
-def integrate_sensor(params, start, stop):
+def integrate_sensor(image, start, stop):
     dx = stop - start
-    baseline_at_start = params["baseline_constant"] + params["baseline_slope"]*start
-    baseline_contribution = dx*baseline_at_start + dx*dx*params["baseline_slope"]/2
-    fiducial_start = params["fiducial_center"] - params["fiducial_width"]/2
-    fiducial_stop = params["fiducial_center"] + params["fiducial_width"]/2
+    baseline_at_start = image["baseline_constant"] + image["baseline_slope"]*start
+    baseline_contribution = dx*baseline_at_start + dx*dx*image["baseline_slope"]/2
+    fiducial_start = image["fiducial_center"] - image["fiducial_width"]/2
+    fiducial_stop = image["fiducial_center"] + image["fiducial_width"]/2
     fiducial_contribution = max(0, min(fiducial_stop, stop) - max(fiducial_start, start))
     return baseline_contribution + fiducial_contribution
 
--- simulate_test.py
+++ simulate_test.py
@@ -7,42 +7,57 @@ from simulate import integrate_sensor, digitize
 @pytest.fixture
 def params():
     return {
-        "num_pixels": 100,
-        "sensor_width": 1,
-        "sensor_spacing": 1,
-        "sensor_min": 0,
-        "sensor_max": 1,
-        "num_grayscale_levels": 2**8,
+        "sensors": {
+            "count": 100,
+            "width": 1,
+            "spacing": 1,
+        },
+        "digitizer": {
+            "sensor_min": 0,
+            "sensor_max": 1,
+            "num_grayscale_levels": 2**8,
+        },
         "sigma": 0,
-        "baseline_constant": 0,
-        "baseline_slope": 0,
-        "fiducial_width": 3,
-        "fiducial_center": 50,
+        "image": {
+            "baseline_constant": 0,
+            "baseline_slope": 0,
+            "fiducial_width": 3,
+            "fiducial_center": 50,
+        },
     }
 
 
-def test_integrate_sensor_no_overlap_no_baseline(params):
-    assert integrate_sensor(params, 0, 1) == 0
+@pytest.fixture
+def image_params(params):
+    return params["image"]
 
 
-def test_integrate_sensor_complete_overlap_no_baseline(params):
-    assert math.isclose(integrate_sensor(params, 50, 51), 1.0)
+def test_integrate_sensor_no_overlap_no_baseline(image_params):
+    assert integrate_sensor(image_params, 0, 1) == 0
 
 
-def test_integrate_sensor_partial_overlap_no_baseline(params):
-    assert math.isclose(integrate_sensor(params, 48, 49), 0.5)
+def test_integrate_sensor_complete_overlap_no_baseline(image_params):
+    assert math.isclose(integrate_sensor(image_params, 50, 51), 1.0)
 
 
-def test_integrate_sensor_partial_overlap_constant_baseline(params):
-    params["baseline_constant"] = 2.0
-    assert math.isclose(integrate_sensor(params, 48, 49), 0.5 + params["baseline_constant"])
+def test_integrate_sensor_partial_overlap_no_baseline(image_params):
+    assert math.isclose(integrate_sensor(image_params, 48, 49), 0.5)
 
 
-def test_integrate_sensor_partial_overlap_linear_baseline(params):
-    params["baseline_slope"] = 2.0
+def test_integrate_sensor_partial_overlap_constant_baseline(image_params):
+    image_params["baseline_constant"] = 2.0
+    actual = integrate_sensor(image_params, 48, 49)
+    expected = 0.5 + image_params["baseline_constant"]
+    assert math.isclose(actual, expected)
+
+
+def test_integrate_sensor_partial_overlap_linear_baseline(image_params):
+    image_params["baseline_slope"] = 2.0
     x0 = 48
     x1 = 49
     dx = x1 - x0
-    triangle = dx*(dx*params["baseline_slope"])/2
-    rectangle = dx*params["baseline_slope"]*x0
-    assert math.isclose(integrate_sensor(params, 48, 49), 0.5 + triangle + rectangle)
+    triangle = dx*(dx*image_params["baseline_slope"])/2
+    rectangle = dx*image_params["baseline_slope"]*x0
+    actual = integrate_sensor(image_params, 48, 49)
+    expected = 0.5 + triangle + rectangle
+    assert math.isclose(actual, expected)
