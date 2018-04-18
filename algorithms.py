738f8e50c4bc34ac4e2c222095d08ef384e79946
Zach Waggoner <zach.waggoner1@gmail.com>
2018-04-18 13:27:54 -0500

    Add a couple 2D algorithms

--- algorithms.py
+++ algorithms.py
@@ -1,7 +1,29 @@
 import numpy as np
+import scipy.ndimage
+from scipy.ndimage import gaussian_filter
 
 
 def center_of_mass(image):
     x = np.arange(image.size, dtype=np.double)
     weights = image.astype(np.double)/np.sum(image)
     return np.sum(weights*x)
+
+
+def center_of_mass_scipy(image):
+    return scipy.ndimage.center_of_mass(image)
+
+
+def gaussian_center_of_mass(image):
+    blurred_image = gaussian_filter(image, 1.5, mode='reflect')
+    return scipy.ndimage.center_of_mass(blurred_image)
+
+
+def gaussian_threshold_center_of_mass(image):
+    blurred_image = gaussian_filter(image, 1.5, mode='reflect')
+    surface = blurred_image.copy()
+    surface[1:-1, 1:-1] = np.nan
+    roi_surface_max = np.nanmax(surface)
+    roi_max = np.max(blurred_image)
+    threshold = roi_surface_max + (roi_max - roi_surface_max) * 0.5
+    labelled_array = (blurred_image > threshold).astype(int)
+    return scipy.ndimage.center_of_mass(blurred_image, labelled_array)[0]
