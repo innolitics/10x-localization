ede91718c010e32747b8399b6c018d30f50caa77
Reece Stevens <reecestevens24@gmail.com>
2018-04-21 11:18:33 -0500

    Add COM+Threshold algorithm

--- algorithms.py
+++ algorithms.py
@@ -5,3 +5,10 @@ def center_of_mass(image):
     x = np.arange(image.size, dtype=np.double)
     weights = image.astype(np.double)/np.sum(image)
     return np.sum(weights*x)
+
+def center_of_mass_with_threshold(image):
+    x = np.arange(image.size, dtype=np.double)
+    threshold = 0.4*np.max(image)
+    image[image<threshold] = 0
+    weights = image.astype(np.double)/np.sum(image)
+    return np.sum(weights*x)
